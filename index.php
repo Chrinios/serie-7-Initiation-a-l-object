2e0924b76c6a1b08a81f1ba86d0e38f960ccdc59
Vincent <102018577+Chrinios@users.noreply.github.com>
2022-06-17 10:34:54 +0200

    Debut exo 1
    Début de l'exercice - avant correction

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php 
+    //ini_set('display_errors',false);
+
+    public class city{
+
+        //défini les objets ville et département.
+        $ville = 'Bordeaux';
+        $departement = 'Gironde';
+
+        echo //continuer
+    }
+?>
